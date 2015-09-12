94fb4fc3c13ead1513b52245c7af58dde9b5dee1
owlas <O.Laslett@soton.ac.uk>
2015-09-12 18:38:05 +0200

    Added documentation to the LE source code

--- src/LangevinEquation.cpp
+++ src/LangevinEquation.cpp
@@ -6,6 +6,9 @@
 //
 #include <LangevinEquation.h>
 
+// Langevin Equation object is required to have a fixed dimension,
+// specify 'd' when constructing an instance.
+
 // Constructor
 LangevinEquation::LangevinEquation(int d)
   : dim(d)
