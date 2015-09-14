d60d6ac19c8e59953546fa347846512de405296c
Oliver Laslett <o.laslett@soton.ac.uk>
2015-09-14 12:22:16 +0100

    Wrote diffusion test for StocLLG and removed tolerance lines from StocLLG tests

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,7 @@ using std::abs;
 #include<gtest/gtest.h>
 #include<boost/multi_array.hpp>
 typedef boost::multi_array<float,1> array_f;
+typedef boost::multi_array<float,2> matrix_f;
 
 // Test construction of StocLLG
 TEST(StochasticLLG, ContructAndDim)
@@ -27,9 +28,6 @@ TEST(StochasticLLG, ContructAndDim)
 // Test deterministic part of StocLLG
 TEST(StochasticLLG, Drift)
 {
-  // Tolerance for solution
-  float tol=1e-5;
-
   // declare in and out arrays
   array_f out( boost::extents[3] );
   array_f in( boost::extents[3] );
@@ -41,11 +39,40 @@ TEST(StochasticLLG, Drift)
   StocLLG llg( 2, 3, 1, 2, 3 );
   llg.computeDrift( out, in );
 
-  // Are the solutions close?
+  // Are the solutions correct?
   EXPECT_EQ( -34, out[0] );
   EXPECT_EQ( -4, out[1] );
   EXPECT_EQ( 20, out[2] );
 }
+
+// Test the stochastic part of the StocLLG
+TEST(StochasticLLG, Diffusion)
+{
+  // declare in and out arrays
+  array_f in( boost::extents[3] );
+  matrix_f out( boost::extents[3][3] );
+  in[0] = 2;
+  in[1] = 3;
+  in[2] = 4;
+
+  // set up the StocLLG and compute diffusion
+  StocLLG llg( 2, 3, 1, 2, 3 );
+  llg.computeDiffusion( out, in );
+  
+  // are the solutions correct?
+  EXPECT_EQ( 150, out[0][0] );
+  EXPECT_EQ( -28, out[0][1] );
+  EXPECT_EQ( -54, out[0][2] );
+  EXPECT_EQ( -44, out[1][0] );
+  EXPECT_EQ( 120, out[1][1] );
+  EXPECT_EQ( -68, out[1][2] );
+  EXPECT_EQ( -42, out[2][0] );
+  EXPECT_EQ( -76, out[2][1] );
+  EXPECT_EQ( 78, out[2][2] );
+}
+
+
+  
 
 int main( int argc, char **argv )
 {
