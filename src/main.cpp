edd25e019cd8410ddaf3b5674f880a63ff54ae9f
Oliver Laslett <o.laslett@soton.ac.uk>
2015-09-14 11:41:13 +0100

    Started writing main.cpp for unit tests. Implemented for the drift of StocLLG

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,47 @@
 using std::cout;
 using std::endl;
 
+#include<cmath>
+using std::abs;
+
 #include<LangevinEquation.hpp>
 #include<StocLLG.hpp>
+#include<gtest/gtest.h>
+#include<boost/multi_array.hpp>
+typedef boost::multi_array<float,1> array_f;
 
-int main(void)
+// Test construction of StocLLG
+TEST(StochasticLLG, ContructAndDim)
 {
-  cout << "Attempting to create StocLLG object..." << endl;
-  StocLLG llg(1,2,3,4,5,6);
-  return 0;
+  StocLLG llg( 1, 2, 3, 4, 5 );
+  EXPECT_EQ( 3, llg.getDim() );
+}
+
+// Test deterministic part of StocLLG
+TEST(StochasticLLG, Drift)
+{
+  // Tolerance for solution
+  float tol=1e-5;
+
+  // declare in and out arrays
+  array_f out( boost::extents[3] );
+  array_f in( boost::extents[3] );
+  in[0] = 2;
+  in[1] = 3;
+  in[2] = 4;
+
+  // Set up StocLLG and compute drift
+  StocLLG llg( 2, 3, 1, 2, 3 );
+  llg.computeDrift( out, in );
+
+  // Are the solutions close?
+  EXPECT_EQ( -34, out[0] );
+  EXPECT_EQ( -4, out[1] );
+  EXPECT_EQ( 20, out[2] );
+}
+
+int main( int argc, char **argv )
+{
+  ::testing::InitGoogleTest( &argc, argv );
+  return RUN_ALL_TESTS();
 }
