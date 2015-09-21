19cd1a92c3b22a48e99db94b06f51ae8bf246d94
Oliver Laslett <o.laslett@soton.ac.uk>
2015-09-21 17:03:51 +0100

    Wrote class for particle clusters

---
+++ include/ParticleCluster.hpp
@@ -0,0 +1,48 @@
+// ParticleCluster.hpp
+// Header file for a cluster of magnetic nanoparticles. Particle
+// cluster is always a constant. Use this to store multiple particles
+// in a group and arranged in three dimensional space.
+//
+// Oliver W. Laslett (2015)
+// O.Laslett@soton.ac.uk
+//
+#ifndef PCLUSTER_H
+#define PCLUSTER_H
+
+#include<Particle.hpp>
+#include<stdexcept>
+#include<cmath>
+#include<vector>
+#include<boost/multi_array.hpp>
+typedef boost::multi_array<float,2> matrix_f;
+typedef boost::multi_array<float,3> array3_f;
+
+
+class ParticleCluster
+{
+public:
+  // constructor
+  // Particle list could be a boost pointer array?
+  // List of N particles and matrix of locations (N x 3)
+  ParticleCluster( std::vector<Particle> list, matrix_f locations );
+
+  // Set the particle list
+  void setParticles( std::vector<Particle> );
+
+  // Set the locations and compute distances
+  void setLocs( matrix_f );
+
+  // getters
+  unsigned int getNParticles() const;
+  matrix_f getLocations() const;
+  array3_f getDistances() const;
+  
+
+private:
+  unsigned int const N;
+  std::vector<Particle> particles;
+  matrix_f locs;
+  array3_f dist;
+};
+#endif
+  
---
+++ lib/ParticleCluster.cpp
@@ -0,0 +1,45 @@
+// ParticleCluster.cpp
+// Implementation of the particle cluster constant class for holding
+// particles in groups in 3d space.
+//
+// Oliver W. Laslett (2015)
+// O.Laslett@soton.ac.uk
+//
+#include<ParticleCluster.hpp>
+
+// constructor
+ParticleCluster::ParticleCluster( std::vector<Particle> list,
+                                  matrix_f locations )
+  : N( int( list.size() ) )
+  , locs( boost::extents[N][3] )
+  , dist( boost::extents[N][N][3] )
+{
+  setParticles( list );
+  setLocs( locations );
+}
+
+// Set the particle list
+void ParticleCluster::setParticles( std::vector<Particle> p )
+{
+  particles = p;
+}
+
+// set the location matrix and check dimensions
+// then compute the relative distances
+void ParticleCluster::setLocs( matrix_f l )
+{
+  if( ( l.shape()[0] != N ) or ( l.shape()[1] != 3 ) )
+    throw std::invalid_argument( "location matrix must be size (Nx3)"
+                                 " where particle list is length N." );
+  locs = l;
+
+  for( array3_f::index i=0; i<N; i++ )
+    for( array3_f::index j=0; j<N; j++ )
+      for( array3_f::index k=0; k<3; k++ )
+        dist[i][j][k] = std::abs( locs[i][k] - locs[j][k] );
+}
+
+// getters
+unsigned int ParticleCluster::getNParticles() const { return N; } 
+matrix_f ParticleCluster::getLocations()  const { return locs; }
+array3_f ParticleCluster::getDistances() const { return dist; }
