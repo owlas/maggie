d32e55f2ed1faf1bbaec4f9cdb33259b546c57e1
Oliver Laslett <o.laslett@soton.ac.uk>
2015-09-23 16:26:45 +0100

    First attempt at Heun class, needs testing

---
+++ include/Heun.hpp
@@ -0,0 +1,45 @@
+// Heun.hpp
+// Header for the Heun integration scheme
+//
+// Oliver W. Laslett 2015
+// O.Laslett@soton.ac.uk
+//
+#ifndef HEUN_H
+#define HEUN_H
+
+#include<Integrator.hpp>
+#include<cmath>
+using std::sqrt;
+#include<boost/random.hpp>
+using boost::variate_generator;
+using boost::mt19937;
+using boost::normal_distribution;
+#include<boost/multi_array.hpp>
+using array_f = boost::multi_array<float,1>;
+using matrix_f = boost::multi_array<float,2>;
+
+class Heun : public Integrator
+{
+  
+public:
+  // Constructor
+  Heun( LangevinEquation &le, array_f& init_state,
+	float time, float dt, mt19937 &rng );
+
+  // Step the integrator once
+  virtual void step();
+
+private:
+  float h;
+  int dim;
+  array_f dw;
+  array_f xPred;
+  array_f tmp1;
+  array_f tmp1Up;
+  array_f wienerProducts;
+  matrix_f tmp2;
+  matrix_f tmp2Up;
+  normal_distribution<float> dist;
+  variate_generator<mt19937 &, normal_distribution<float> > gen;
+};
+#endif
---
+++ lib/Heun.cpp
@@ -0,0 +1,59 @@
+// Heun.cpp
+// Implementation for the Heun integration scheme
+//
+// Oliver W. Laslett 2015
+// O.Laslett@soton.ac.uk
+//
+#include<Heun.hpp>
+
+
+// Constructor
+Heun::Heun( LangevinEquation &le, array_f& init_state,
+	    float time, float dt, mt19937& rng )
+  : Integrator( le, init_state, time)
+  , h( dt )
+  , dim( le.getDim() )
+  , dw( boost::extents[dim])
+  , xPred( boost::extents[dim])
+  , tmp1( boost::extents[dim])
+  , tmp1Up( boost::extents[dim])
+  , wienerProducts( boost::extents[dim])
+  , tmp2( boost::extents[dim][dim] )
+  , tmp2Up( boost::extents[dim][dim] )
+  , dist(0,1)
+  , gen( rng, dist )
+{
+  // empty
+}
+
+void Heun::step()
+{
+  for( int i=0; i<dim; i++ )
+    dw[i] = sqrt(h) * gen();
+  
+  getLE().computeDrift( tmp1, state );
+  getLE().computeDiffusion( tmp2, state );
+  
+  for( int i=0; i<dim; i++ )
+    {
+      wienerProducts[i] = 0;
+      for( int j=0; j<dim; j++ )
+	wienerProducts[i] += tmp2[i][j]*dw[j];
+      xPred[i] = state[i] + tmp1[i]*h + wienerProducts[i];
+    }
+
+  getLE().computeDrift( tmp1Up, xPred );
+  getLE().computeDiffusion( tmp2Up, xPred );
+  
+  for( int i=0; i<dim; i++ )
+    {
+      for( int j=0; j<dim; j++ )
+	wienerProducts[i] += tmp2Up[i][j]*dw[j];
+      wienerProducts[i] *= 0.5;
+      xPred[i] = state[i] + 0.5*h*( tmp1[i] + tmp1Up[i] )
+	+ wienerProducts[i];
+    }
+  
+  setState( xPred );
+  setTime( getTime()+h );
+}
