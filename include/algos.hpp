dee18529a241b05b9c3ddb7a6abb95e9b6f9a8ab
owlas <ollielaslett@icloud.com>
2016-11-12 11:40:28 +0000

    Add end condition to interpolate func

--- include/algos.hpp
+++ include/algos.hpp
@@ -11,20 +11,22 @@ namespace algos
     template<class Container>
     void interpolate_to_regular_line( Container const& values,
                                       Container const& grid_points,
-                                      Container& regular_grid_values )
+                                      const size_t Nvalues,
+                                      Container& regular_grid_values,
+                                      const size_t Nregular_values )
     {
         // calculate regular grid spacing
-        double spacing = grid_points.back() / ( regular_grid_values.size() - 1 );
+        double spacing = grid_points.back() / ( Nregular_values - 1 );
 
         // add start and end conditions
         regular_grid_values.front() = values.front();
-        regular_grid_values.back() = values.back();
+        regular_grid_values[Nregular_values-1] = values[Nvalues-1];
 
         // track how many points have been written
         unsigned int points_written=1;
 
         // map each point in the irregular grid onto the regular grid
-        for( unsigned int i=1; i!=values.size(); ++i )
+        for( unsigned int i=1; i!=Nvalues; ++i )
             while( points_written*spacing <= grid_points[i] )
             {
                 regular_grid_values[points_written] = values[i-1]
@@ -33,6 +35,15 @@ namespace algos
                     * ( values[i] - values[i-1] );
                 points_written++;
             }
+    }
+
+    template<class Container>
+    void interpolate_to_regular_line( Container const& values,
+                                      Container const& grid_points,
+                                      Container& regular_grid_values )
+    {
+        interpolate_to_regular_line( values, grid_points, values.size(),
+                                     regular_grid_values, regular_grid_values.size() );
     }
 }
 
